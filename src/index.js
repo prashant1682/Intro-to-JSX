3dac13df2f5bf6278c207f2fdb1bd9a70af42efd
prashant1682 <prashant009@gmail.com>
2020-12-28 10:27:53 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+//import work just like require in reactJs
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+// ReactDOM.render(what to update , where to update) -- this will render a HTML element using JS
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
